93434a93011fc888c1399bd7e64c698aaff774ea
Anirudh Raturi <annigeek@gmail.com>
2025-10-06 22:51:39 +0530

    datatypes in java

---
+++ 1/2DataTypes.java
@@ -0,0 +1,20 @@
+class DataTypes{
+    public static void main(String a[]){
+        //INTEGER
+        //int => 4bytes , short =>2bytes , long => 8bytes , byte => 1byte => -2^7 to 2^7-1 i.e -128 to 127
+        int num1 = 9;
+        // byte num2 = 128; //   it will not exicute since its range is between -128 to 127
+        byte num2 = 127;
+    
+        //FLOAT 
+        //in float datatype the default value is double so if we are using float datatype then we have to put f in the end of the number
+        float f = 5.8f;    //4bytes
+        double d = 5.87;   //8 bytes
+
+        //CHARACTER
+        char c = 'A';
+
+        boolean b = true;
+    }
+
+}
